44fc0973d9433874dadd949237c394d56587937f
Yu Zhao <zhaoyu9610@hotmail.com>
2018-10-14 02:49:59 -0400

    add middleware for CORS header

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'SuperCanvasser.middleware.CORS_Middleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'

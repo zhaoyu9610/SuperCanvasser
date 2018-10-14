b5252fb53431e548368a776c14338df3f70daf34
Yu Zhao <zhaoyu9610@hotmail.com>
2018-10-14 11:26:27 -0400

    fix typo in admin.py

--- backend/SuperCanvasser/admin.py
+++ backend/SuperCanvasser/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from SuperCanvasser.models import *
 
-admin.site.register([User, Availability. Campaign, CampaignDate, Assignment, Result,
+admin.site.register([User, Availability, Campaign, CampaignDate, Assignment, Result,
                      Answer, Location, Parameter])
 

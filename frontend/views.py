38517225189824ecf8434db6fc34a33377f6632b
Babu <babursha.saleem@stonybrook.edu>
2018-10-19 17:39:59 -0400

    added return to all requests

--- frontend/views.py
+++ frontend/views.py
@@ -5,35 +5,35 @@ from django.shortcuts import render
 
 def login(request):
     data = {}
-    render(request, 'login.html', data)
+    return render(request, 'login.html', data)
 
 
 def account(request):
     data = {}
-    render(request, 'account.html', data)
+    return render(request, 'account.html', data)
 
 
 def manager(request):
     data = {}
-    render(request, 'manager.html', data)
+    return render(request, 'manager.html', data)
 
 
 def campaign(request):
     data = {}
-    render(request, 'campaign.html', data)
+    return render(request, 'campaign.html', data)
 
 
 def canvasser(request):
     data = {}
-    render(request, 'canvasser.html', data)
+    return render(request, 'canvasser.html', data)
 
 
 def assignment(request):
     data = {}
-    render(request, 'assignment.html', data)
+    return render(request, 'assignment.html', data)
 
 
 def current(request):
     data = {}
-    render(request, 'assignment.html', data)
+    return render(request, 'assignment.html', data)
 

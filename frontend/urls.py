86c69d4f0c3701ed4ac45af87d72fc42568097ed
Yu Zhao <zhaoyu9610@hotmail.com>
2018-10-19 17:23:03 -0400

    setup urls and html pages for frontend

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('campaigns', views.index, name='index'),
+    path('', views.login, name='login'),
+    path('account', views.account, name='account'),
+    path('manager', views.manager, name='manager home page'),
+    path('manager/campaigns/<int:id>', views.campaign, name='campaign page'),
+    path('canvasser', views.canvasser, name='canvasser home page'),
+    path('canvasser/assignments/<int:id>', views.assignment, name='assignments page'),
+    path('canvasser/assignments/current', views.current, name='current assignment')
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -3,5 +3,37 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def index(request):
-    return render(request, "index.html")
+def login(request):
+    data = {}
+    render(request, 'login.html', data)
+
+
+def account(request):
+    data = {}
+    render(request, 'account.html', data)
+
+
+def manager(request):
+    data = {}
+    render(request, 'manager.html', data)
+
+
+def campaign(request):
+    data = {}
+    render(request, 'campaign.html', data)
+
+
+def canvasser(request):
+    data = {}
+    render(request, 'canvasser.html', data)
+
+
+def assignment(request):
+    data = {}
+    render(request, 'assignment.html', data)
+
+
+def current(request):
+    data = {}
+    render(request, 'assignment.html', data)
+

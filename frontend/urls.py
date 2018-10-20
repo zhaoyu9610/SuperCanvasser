a8a4a3a06105084a432b0b08cdf0262194048183
Yu Zhao <zhaoyu9610@hotmail.com>
2018-10-19 21:38:40 -0400

    modify urls, and templates in views set up access control in utils add functions

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,9 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('account', views.account, name='account'),
-    path('manager', views.manager, name='manager home page'),
-    path('manager/campaigns/<int:id>', views.campaign, name='campaign page'),
-    path('canvasser', views.canvasser, name='canvasser home page'),
-    path('canvasser/assignments/<int:id>', views.assignment, name='assignments page'),
-    path('canvasser/assignments/current', views.current, name='current assignment')
-]
+    path('campaigns', views.campaigns, name='table for campaigns'),
+    path('/campaigns/<int:cid>', views.campaign, name='view page for campaign with id'),
+    path('/campaigns/<int:cid>/edit', views.campaign_edit, name='edit page for campaign with id'),
+    path('/campaigns/<int:cid>/assignments', views.campaign_assignments, name='table for assignments for the campaigns'),
+    path('/campaigns/<int:cid>/assignments/<int:aid>', views.campaign_assignment, name='view page for an assignment'),
+    path('/campaigns/create', views.campaign_create, name='create page for campaign'),
+    path('/assignments', views.canvasser_assignments, name='table for assignments for a canvasser'),
+    path('/assignments/<int:aid>', views.canvasser_assignment, name='view page for an assignment'),
+    path('/assignments/current', views.current_assignment, name='view page for current assignment'),
+    path('/admin', views.admin, name='home page for admin')]
--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from . import utils
 
 
 def login(request):
@@ -9,31 +8,129 @@ def login(request):
 
 
 def account(request):
-    data = {}
-    return render(request, 'account.html', data)
+    if 'cookie' in request.COOKIES:
+        uid = request.COOKIES['cookie']
+        data = {'role': utils.getRoles(uid)}
+        return render(request, 'account.html', data)
+    else:
+        return redirect('login')
 
 
-def manager(request):
-    data = {}
-    return render(request, 'manager.html', data)
+def admin(request):
+    if 'cookie' in request.COOKIES:
+        uid = request.COOKIES['cookie']
+        roles = utils.getRoles(uid)
+        if roles[0]:
+            data = {'role': roles}
+            return render(request, 'admin.html', data)
+        return render(request, 'error.html', utils.generate_error_data(request, ''))
+    else:
+        return redirect('login')
 
 
-def campaign(request):
-    data = {}
-    return render(request, 'campaign.html', data)
+def campaigns(request):
+    if 'cookie' in request.COOKIES:
+        uid = request.COOKIES['cookie']
+        roles = utils.getRoles(uid)
+        if roles[1]:
+            data = {'role': roles}
+            return render(request, 'campaigns.html', data)
+        return render(request, 'error.html', utils.generate_error_data(request, ''))
+    else:
+        return redirect('login')
 
 
-def canvasser(request):
-    data = {}
-    return render(request, 'canvasser.html', data)
+def campaign(request, cid):
+    if 'cookie' in request.COOKIES:
+        uid = request.COOKIES['cookie']
+        roles = utils.getRoles(uid)
+        if roles[1]:
+            data = {'role': roles}
+            return render(request, 'campaign.html', data)
+        return render(request, 'error.html', utils.generate_error_data(request, ''))
+    else:
+        return redirect('login')
 
 
-def assignment(request):
-    data = {}
-    return render(request, 'assignment.html', data)
+def campaign_edit(request, cid):
+    if 'cookie' in request.COOKIES:
+        uid = request.COOKIES['cookie']
+        roles = utils.getRoles(uid)
+        if roles[1]:
+            data = {'role': roles}
+            return render(request, 'campaign_edit.html', data)
+        return render(request, 'error.html', utils.generate_error_data(request, ''))
+    else:
+        return redirect('login')
 
 
-def current(request):
-    data = {}
-    return render(request, 'assignment.html', data)
+def campaign_assignments(request, cid):
+    if 'cookie' in request.COOKIES:
+        uid = request.COOKIES['cookie']
+        roles = utils.getRoles(uid)
+        if roles[1]:
+            data = {'role': roles}
+            return render(request, 'assignments.html', data)
+        return render(request, 'error.html', utils.generate_error_data(request, ''))
+    else:
+        return redirect('login')
 
+
+def campaign_assignment(request, cid, aid):
+    if 'cookie' in request.COOKIES:
+        uid = request.COOKIES['cookie']
+        roles = utils.getRoles(uid)
+        if roles[1]:
+            data = {'role': roles}
+            return render(request, 'assignment.html', data)
+        return render(request, 'error.html', utils.generate_error_data(request, ''))
+    else:
+        return redirect('login')
+
+
+def campaign_create(request):
+    if 'cookie' in request.COOKIES:
+        uid = request.COOKIES['cookie']
+        roles = utils.getRoles(uid)
+        if roles[1]:
+            data = {'role': roles}
+            return render(request, 'campaign_create.html', data)
+        return render(request, 'error.html', utils.generate_error_data(request, ''))
+    else:
+        return redirect('login')
+
+
+def canvasser_assignments(request):
+    if 'cookie' in request.COOKIES:
+        uid = request.COOKIES['cookie']
+        roles = utils.getRoles(uid)
+        if roles[1]:
+            data = {'role': roles}
+            return render(request, 'assignments.html', data)
+        return render(request, 'error.html', utils.generate_error_data(request, ''))
+    else:
+        return redirect('login')
+
+
+def canvasser_assignment(request, id):
+    if 'cookie' in request.COOKIES:
+        uid = request.COOKIES['cookie']
+        roles = utils.getRoles(uid)
+        if roles[1]:
+            data = {'role': roles}
+            return render(request, 'assignment.html', data)
+        return render(request, 'error.html', utils.generate_error_data(request, ''))
+    else:
+        return redirect('login')
+
+
+def current_assignment(request):
+    if 'cookie' in request.COOKIES:
+        uid = request.COOKIES['cookie']
+        roles = utils.getRoles(uid)
+        if roles[1]:
+            data = {'role': roles}
+            return render(request, 'assignment.html', data)
+        return render(request, 'error.html', utils.generate_error_data(request, ''))
+    else:
+        return redirect('login')

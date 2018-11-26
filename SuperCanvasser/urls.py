a2084ffc8c88011dcaa364617b1a6047af7de5af
Yu Zhao <zhaoyu9610@hotmail.com>
2018-11-25 21:51:41 -0500

    update static file

--- SuperCanvasser/urls.py
+++ SuperCanvasser/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('backend.urls')),
     path('', include('frontend.urls')),
 ]
+urlpatterns += staticfiles_urlpatterns()
